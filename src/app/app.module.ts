b8d2896fb0801dc6624e7d910ef8ee86789cb52b
AJEvbank <austin.eubank@ttu.edu>
2018-03-11 14:35:00 -0500

    Reset app module file and root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,41 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { LoginPage } from '../pages/login/login';
+import { MainPage } from '../pages/main/main';
+import { DailyNotificationsPage } from '../pages/daily-notifications/daily-notifications';
+import { HighRiskListPage } from '../pages/high-risk-list/high-risk-list';
+import { ShelfHelperPage } from '../pages/shelf-helper/shelf-helper';
+
+import { ItemRecordPage } from '../pages/item-record/item-record';
+import { EditItemRecordPage } from '../pages/item-record/edit-item-record/edit-item-record';
+import { CreateNotificationPage } from '../pages/item-record/create-notification/create-notification';
+import { ThrowawayPage } from '../pages/item-record/throwaway/throwaway';
+
+import { ReportsPage } from '../pages/reports/reports';
+import { CalendarPage } from '../pages/reports/calendar/calendar';
+import { LossOverTimeReportPage } from '../pages/reports/loss-over-time-report/loss-over-time-report';
+import { ReportSpecificationsPage } from '../pages/reports/report-specifications/report-specifications';
+import { ShrinkListPage } from '../pages/reports/shrink-list/shrink-list';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
+    MainPage,
+    DailyNotificationsPage,
+    HighRiskListPage,
+    ItemRecordPage,
+    ReportsPage,
+    EditItemRecordPage,
+    CreateNotificationPage,
+    ThrowawayPage,
+    CalendarPage,
+    LossOverTimeReportPage,
+    ReportSpecificationsPage,
+    ShrinkListPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +48,19 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    MainPage,
+    DailyNotificationsPage,
+    HighRiskListPage,
+    ItemRecordPage,
+    ReportsPage,
+    EditItemRecordPage,
+    CreateNotificationPage,
+    ThrowawayPage,
+    CalendarPage,
+    LossOverTimeReportPage,
+    ReportSpecificationsPage,
+    ShrinkListPage
   ],
   providers: [
     StatusBar,
