2df5b2f635eef0ead1fc0950d53ff0199b4d4097
AJEvbank <austin.eubank@ttu.edu>
2018-03-12 11:09:50 -0500

    working on shelf helper

--- src/services/shelf-helper.service.ts
+++ src/services/shelf-helper.service.ts
@@ -1,3 +1,43 @@
+import { ToGetItem } from '../assets/models/to-get-item.model';
+
+import { File } from '@ionic-native/file';
+import { Storage } from '@ionic/storage';
+import { Injectable, OnInit } from '@angular/core';
+
+@Injectable()
 export class ShelfHelperService {
-  
+
+  shelfHelperList: ToGetItem [];
+  toGetItem: ToGetItem;
+  index: number;
+
+  constructor(private file: File,
+              private storage: Storage) {
+
+  }
+
+  addItem(item: ToGetItem) {
+    this.shelfHelperList.push(item);
+    this.storage.set('shelfHelperList',this.shelfHelperList)
+    .then()
+    .catch(
+      (err) => {
+        console.log(err);
+        this.shelfHelperList.splice(this.shelfHelperList.indexOf(item),1);
+      }
+    );
+  }
+
+  removeItem(index: number) {
+    const itemSave: ToGetItem = this.shelfHelperList.slice(index,index + 1)[0];
+    this.shelfHelperList.splice(index, 1);
+    this.storage.set('shelfHelperList',this.shelfHelperList)
+    .then()
+    .catch(
+      (err) => {
+        console.log(err);
+        this.addItem(itemSave);
+      }
+    )
+  };
 }
