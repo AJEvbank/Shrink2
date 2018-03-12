95cb9e3cd56f696f50779fc7d81274ef57360a4e
AJEvbank <austin.eubank@ttu.edu>
2018-03-12 11:27:37 -0500

    High risk list finished for now.

--- src/services/high-risk-list.service.ts
+++ src/services/high-risk-list.service.ts
@@ -8,10 +8,55 @@ import { HighRiskItem } from '../assets/models/high-risk-item.model';
 export class HighRiskListService {
 
   highRiskList: HighRiskItem [];
+  highRiskItem: HighRiskItem;
+  index: number;
 
   constructor(private storage: Storage,
               private file: File) {}
 
-  
+  addItem(item: HighRiskItem) {
+    this.highRiskList.push(item);
+    this.storage.set('highRiskList',this.highRiskList)
+    .then(
+      // REMINDER: Push to server.
+    )
+    .catch(
+      (err) => {
+        console.log(err);
+        this.highRiskList.splice(this.highRiskList.indexOf(item,1),1);
+      }
+    );
+  }
+
+  removeItem(index: number) {
+    const itemSave: HighRiskItem = this.highRiskList.slice(index, index + 1)[0];
+    this.highRiskList.splice(index, 1);
+    this.storage.set('shelfHelperList',this.highRiskList)
+    .then(
+      // REMINDER: Push to server.
+    )
+    .catch(
+      (err) => {
+        console.log(err);
+        this.highRiskList.push(itemSave);
+      }
+    )
+  }
+
+  fetchList() {
+    return this.storage.get('highRiskList')
+    .then(
+      (list: HighRiskItem []) => {
+        this.highRiskList = list != null ? list: [];
+        return this.highRiskList;
+      }
+    )
+    .catch(
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
+
 
 }
