03dc2f5c8d4a57e559d5299ee7b7a683a9c19230
Cyrus67 <nickalvarez123@gmail.com>
2018-03-14 13:31:03 -0500

    Basic Reports Navigation
    Using same page for each specification. Could probably do this via modals actually...

--- src/pages/reports/report-specifications/report-specifications.ts
+++ src/pages/reports/report-specifications/report-specifications.ts
@@ -1,25 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ReportSpecificationsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-report-specifications',
   templateUrl: 'report-specifications.html',
 })
 export class ReportSpecificationsPage {
+  reportType: string;
+  templateTitle = "Default Report Specifications";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams: NavParams) {
+    //console.log(this.navParams.data);
+    //console.log(this.reportType == ReportType.shrinkList);
+    this.reportType = this.navParams.data;
+    this.templateTitle = this.reportType + " Specifications";
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ReportSpecificationsPage');
-  }
-
 }
