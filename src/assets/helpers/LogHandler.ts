2db0147f671dc64bf47f4988c6768ca844f746a6
AJEvbank <austin.eubank@ttu.edu>
2018-05-02 15:58:56 -0500

    Small modifications...

--- src/assets/helpers/LogHandler.ts
+++ src/assets/helpers/LogHandler.ts
@@ -10,26 +10,26 @@ export class LogHandler {
   }
   public logCont(content: any, callerName: string) : void {
     if (this.active) {
-      console.log("START OUTPUT");
+      console.log("[START OUTPUT]");
       console.log("In " + this.className + " method: " + callerName + " data = " + JSON.stringify(content));
-      console.log("END OUTPUT");
+      console.log("[END OUTPUT]");
     }
   }
 
   public logErr(error: any, callerName: string) : void {
     if (this.active) {
-      console.log("START OUTPUT");
+      console.log("[START OUTPUT]");
       console.log("In " + this.className + " method: " + callerName + ": error = " + JSON.stringify(error, Object.getOwnPropertyNames(error)));
-      console.log("END OUTPUT");
+      console.log("[END OUTPUT]");
     }
   }
 
   public logURLInfo(url: string, body: any, method: string) {
     if (this.active) {
-      console.log("START OUTPUT");
+      console.log("[START OUTPUT]");
       console.log("Method = " + method + " URL: " + url);
       if (body != null) console.log("body: " + JSON.stringify(body));
-      console.log("END OUTPUT");
+      console.log("[END OUTPUT]");
     }
   }
 
